83cd5a555aa7dd7ed1a73194dff27634769ccad8
domikkkk <domik141202@gmail.com>
2023-07-05 21:36:35 +0200

    Added way to look

--- Ikariam/C/c.py
+++ Ikariam/C/c.py
@@ -1,9 +1,10 @@
 import ctypes
+import os
 
-try:
-    c = ctypes.CDLL('./ika.so')
-except Exception:
-    c = ctypes.CDLL('./Ikariam/ika.so')
+
+c = ctypes.CDLL(os.getcwd() + '/ika.so')
+
+
 class island(ctypes.Structure):
     _fields_ = [
         ('x', ctypes.c_int),
@@ -14,44 +15,54 @@ class island(ctypes.Structure):
 class Bonus:
     def __init__(self) -> None:
         self.auto()
+        self.counter = 0
 
     def auto(self):
-        self.keys = {
-            0: [(0, 0)],
-            1: [(0, 1)],
-            2: [(0, 2)],
-            3: [(0, 3)],
-            4: [(0, 4)],
-            5: [(1, 0)],
-            6: [(1, 1)],
-            7: [(1, 2)],
-            8: [(1, 3), (2, 0)],
-            9: [(1, 4)],
-            10:[(2, 1)],
-            11:[(2, 2), (3, 0)],
-            12:[(2, 3)],
-            13:[(3, 1)],
-            14:[(2, 4), (4, 0)],
-            15:[(3, 2)],
-            16:[(4, 1)],
-            17:[(3, 3)],
-            18:[(3, 4), (4, 2)],
-            19:[(4, 3)],
-            20:[(4, 4)]
+        self.result = {
+            0: [(0, 0), None],
+            1: [(0, 1), None],
+            2: [(0, 2), None],
+            3: [(0, 3), None],
+            4: [(0, 4), None],
+            5: [(1, 0), None],
+            6: [(1, 1), None],
+            7: [(1, 2), None],
+            8: [((1, 3), (2, 0)), None],
+            9: [(1, 4), None],
+            10: [(2, 1), None],
+            11: [((2, 2), (3, 0)), None],
+            12: [(2, 3), None],
+            13: [(3, 1), None],
+            14: [((2, 4), (4, 0)), None],
+            15: [(3, 2), None],
+            16: [(4, 1), None],
+            17: [(3, 3), None],
+            18: [((3, 4), (4, 2)), None],
+            19: [(4, 3), None],
+            20: [(4, 4), None]
         }
         return
 
+    def _parse(self, cords: island):
+        if cords.x == -1 and cords.y == -1:
+            if not self.result[self.counter][1]:
+                del self.result[self.counter]
+            self.counter += 1
+        else:
+            if not self.result[self.counter][1]:
+                self.result[self.counter][1] = []
+            x = self.result[self.counter][1]
+            x.append((cords.x, cords.y))
+
     def parse(self, arg):
-        ret = ""
-        for a, b in arg:
-            if a == 0:
-                ret += f'Ładowność +{round(b/6*100, 1)}%, '
-            elif a == 1:
-                ret += f'Delfin +{100}% i ładowność +{round(b/6 * 100, 1)}%, '
-            else:
-                ret += f'Delfin +100%, tryton +{(a-1)*100}% i ładowność +{round(b/6*100, 1)}%, '
-        return ret + ':'
-            
+        for x in range(100):
+            if arg[x].x == 0:
+                if not self.result[self.counter][1]:
+                    del self.result[self.counter]
+                return self.result
+            self._parse(arg[x])
+        return self.result
+
 
 c.distance.argtypes = (island, island)
 c.distance.restype = ctypes.c_float
@@ -75,20 +86,16 @@ qsort = c.qqsort
 
 
 import sys
+
+
 def f():
     arg = sys.argv[1:]
     arg = [int(s) for s in arg]
-    b = 0
     a = find(island(arg[0], arg[1]), calc_time(arg[2], arg[3], arg[4]))
     bonus = Bonus()
-    for x in range(100):
-        if a[x].x == 0:
-            break
-        if a[x].x != -1:
-            print(f"{a[x].x}, {a[x].y}")
-        else:
-            b += 1
-            print(f'{bonus.parse(bonus.keys[b])}')
+    a = bonus.parse(a)
+    print(a)
+
 
 f()
 
