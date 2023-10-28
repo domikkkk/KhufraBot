f085b14c64823aa5b7c9e2c527ace4289187ef56
Dominik Sidorczuk <domik141202@gmail.com>
2023-10-29 01:03:53 +0200

    Finding islands for fire ships works

--- Ikariam/C/queue.c
+++ Ikariam/C/queue.c
@@ -11,31 +11,79 @@ int compare_time(const void *a, const void *b){
 }
 
 
-miotly *czasy(island *isl, int n, island dest) {
+pair czasy(island *isl, int n, island dest) {
     miotly *m = calloc(n, sizeof(miotly));
     for (int i=0;i<n;i++) {
         m[i].i = &isl[i];
         m[i].t = distance(isl[i], dest) * M;
     }
     qsort(m, n, sizeof(miotly), compare_time);
-    int x = ciag(m, n);
-    printf("%d\n", x);
-    return m;
+    miotly *odp = ciag(m, n);
+    free(m);
+    n = (int)odp->t;
+    filter(odp+1, &n);
+    for (int i=0; i<n; i++) {
+        odp[i] = odp[i+1];
+    }
+    odp = realloc(odp, n * sizeof(miotly));
+    pair p;
+    p.m = odp;
+    p.n = n;
+    return p;
 }
 
 
-int ciag(miotly *m, int n) {
+miotly *ciag(miotly *m, int n) {
     int max_n = 0;
+    miotly *isl;
     for (int i=0; i<n; i++) {
         int _n = 0;
-        for (int j=0; j<n; i++) {
+        for (int j=0; j<n; j++) {
             float odl = fabs(m[j].t - m[i].t);
-            int nn = odl / M;
+            int nn = rest(odl);
             if (odl <= nn * M + E_M && odl >= nn * M - E_M) {
                 _n++;
             }
         }
-        max_n = fmax(_n, max_n);
+        if (_n > max_n) {
+            isl = &m[i];
+        }
+        max_n = max(_n, max_n);
     }
-    return max_n;
+    miotly *odp = calloc(max_n+1, sizeof(miotly));
+    odp->t = (float)max_n;
+    odp++;
+    for (int i=0; i<n; i++) {
+        float odl = fabs(isl->t - m[i].t);
+        int nn = rest(odl);
+        if (odl <= nn * M + E_M && odl >= nn * M - E_M) {
+            odp->i = m[i].i;
+            odp->t = m[i].t;  // make a copy
+            odp++;
+        }
+    }
+    return odp-max_n-1;
+}
+
+
+int rest(float odl) {
+    if (odl > M + E_M) goto ret1;
+    if (odl > M - E_M) goto ret2;
+    return 0;
+ret1:
+    return odl / M + rest(odl / (float)M);
+ret2:
+    return 1;
+}
+
+
+void filter(miotly *m, int *n) {
+    int j = 0;
+    for (int i=0; i<*n-1; i++) {
+        if (m[i+1].t - m[i].t > M - E_M) {
+            m[j++] = m[i];
+        }
+    }
+    m[j++] = m[*n-1];
+    *n = j;
 }
--- Ikariam/C/queue.h
+++ Ikariam/C/queue.h
@@ -2,7 +2,8 @@
 
 
 #define M 30
-#define E_M 1
+#define E_M 3  // epsilon
+#define max(a, b) ((a)>(b)?(a):(b))
 
 
 typedef struct {
@@ -10,6 +11,15 @@ typedef struct {
     float t;
 } miotly;
 
+
+typedef struct {
+    miotly *m;
+    int n;
+} pair;
+
+
 int compare_time(const void *a, const void *b);
-miotly *czasy(island *isl, int n, island dest);
-int ciag(miotly *m, int n);
+int rest(float odl);
+pair czasy(island *isl, int n, island dest);
+miotly *ciag(miotly *m, int n);
+void filter(miotly *m, int *n);
