1050eeb82630fc2a88a5f6eeee0052952c3fc295
Dominik <domik141202@gmail.com>
2025-11-20 00:24:38 +0100

    add header for __init__

--- Ikariam/__init__.py
+++ Ikariam/__init__.py
@@ -2,6 +2,7 @@ from .queue import calc, Wyspy
 from .Koszty import Composition, upkeep_h
 from .api.session import IkaBot, ExpiredSession
 from .api.generalBot import General
+from .api.homeSecretaryBot import HomeSecretary
 from .api.rgBot import rgBot
 from .api.lookerOnIsland import Planter
 from .Islands import Player, Island, Map, City
